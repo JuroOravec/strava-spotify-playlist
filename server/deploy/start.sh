afefdbe2876fd633016de686516773611679f5a4
Juro Oravec <juraj.oravec.josefson@gmail.com>
2021-01-16 22:35:53 +0000

    ci: update CodeDeploy ApplicationStart hook to run in prod

--- server/deploy/start.sh
+++ server/deploy/start.sh
@@ -3,4 +3,4 @@ cd ~/app/server
 npm ci
 npm run build
 
-npx pm2 start dist/index.js -i 0 --name="strava-spotify-playlist"
+NODE_ENV=production npx pm2 start dist/index.js -i 0 --name="strava-spotify-playlist"
