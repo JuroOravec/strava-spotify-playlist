e1c0583a58acaf4b6806dca219f617ac93b235f8
Juro Oravec <juraj.oravec.josefson@gmail.com>
2021-01-16 13:10:34 +0000

    refactor: do not install gcc-cpp and make on before_install

--- server/deploy/before_install.sh
+++ server/deploy/before_install.sh
@@ -6,6 +6,5 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 node -e "console.log('Running Node.js ' + process.version)"
-sudo yum install -y gcc-c++ make
 # Reverse proxy
 amazon-linux-extras install -y nginx1
