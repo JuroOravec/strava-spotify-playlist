139b2af93f9886851fa1a57797feffc5b9bd8db7
Juro Oravec <juraj.oravec.josefson@gmail.com>
2021-01-24 14:51:20 +0000

    refactor: update main app file to import install module

--- moovin-groovin-web/src/main.ts
+++ moovin-groovin-web/src/main.ts
@@ -1,13 +1,33 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import vuetify from "./plugins/vuetify";
+import Vue from 'vue';
 
-Vue.config.productionTip = false;
+import registerServiceWorker from './registerServiceWorker';
+import installBase from './plugins/base';
+import installCompositionAPI from './plugins/compositionAPI';
+import installRouter from './plugins/router';
+import installVuetify from './plugins/vuetify';
+import createRoutes from './modules/install/routes';
+import App from './modules/install/components/App.vue';
+
+if (process.env.NODE_ENV === 'production') {
+  registerServiceWorker({
+    baseUrl: process.env.BASE_URL,
+  });
+}
+
+installBase(Vue);
+installCompositionAPI(Vue);
+const vuetify = installVuetify(Vue);
+const router = installRouter(Vue, {
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: createRoutes(),
+});
+
+// TODO: Add VueApollo and configure apollo client
 
 new Vue({
+  name: 'VueApp',
   router,
   vuetify,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App),
+}).$mount('#app');
