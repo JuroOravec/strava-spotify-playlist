23f5fd63f1abaa5dce6fe2813ca5316d30423c38
Juro Oravec <juraj.oravec.josefson@gmail.com>
2021-01-30 13:28:34 +0000

    feat: add profile page util components

---
+++ moovin-groovin-web/src/modules/profile/components/ProfileCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card class="ProfileCard">
+    <v-row class="ProfileCard__header">
+      <v-col cols="auto" class="px-0">
+        <v-card-title>
+          <slot name="title" />
+        </v-card-title>
+      </v-col>
+      <slot name="title-append" />
+    </v-row>
+
+    <v-card-text class="ProfileCard__body">
+      <v-row>
+        <slot name="body" />
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+const ProfileCard = defineComponent({
+  name: 'ProfileCard',
+});
+
+export default ProfileCard;
+</script>
+
+<style lang="scss">
+@import '@/plugins/vuetify/vuetify';
+.ProfileCard {
+  @extend .elevation-3, .pa-3;
+
+  &__header {
+    @extend .my-3, .mx-0;
+
+    .col {
+      padding-bottom: 0;
+      padding-top: 0;
+    }
+    .v-card__title {
+      padding-bottom: 0;
+      padding-top: 0;
+    }
+  }
+}
+</style>
---
+++ moovin-groovin-web/src/modules/profile/components/WarningIrreversible.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="WarningIrreversible">
+    <v-icon color="red lighten-2">warning</v-icon>
+    <span class="body-2 pl-2">This action cannot be undone. </span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+const WarningIrreversible = defineComponent({
+  name: 'WarningIrreversible',
+});
+
+export default WarningIrreversible;
+</script>
