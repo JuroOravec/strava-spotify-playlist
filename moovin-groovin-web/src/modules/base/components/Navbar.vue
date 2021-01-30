025ecda15853ebbd43f0c52851ff7876220e70c2
Juro Oravec <juraj.oravec.josefson@gmail.com>
2021-01-30 13:28:06 +0000

    feat: polish navbar

--- moovin-groovin-web/src/modules/base/components/Navbar.vue
+++ moovin-groovin-web/src/modules/base/components/Navbar.vue
@@ -1,28 +1,31 @@
 <template>
   <v-app-bar class="Navbar" app color="primary" dark flat>
     <v-container class="py-0 fill-height">
-      <v-row justify="space-between">
-        <v-col cols="auto">
-          <span class="title"> MoovinGroovin </span>
+      <v-row class="fill-height" justify="space-between" align="center">
+        <v-col cols="auto" class="Navbar__title fill-height">
+          <router-link to="/" class="fill-height">
+            <v-app-bar-title class="title fill-height d-flex align-center">
+              MoovinGroovin
+            </v-app-bar-title>
+          </router-link>
         </v-col>
 
         <v-spacer></v-spacer>
 
-        <v-col cols="auto">
+        <v-col cols="auto" class="Navbar__nav fill-height">
           <router-link
             v-for="link in navbarLinks"
             :key="'navbar-item-' + link.to.name"
+            class="Navbar__nav-item fill-height"
             :to="link.to"
           >
-            <v-btn text>
+            <v-btn text class="fill-height">
               {{ link.title }}
             </v-btn>
           </router-link>
         </v-col>
 
-        <!-- <v-spacer></v-spacer> -->
-
-        <v-col cols="auto">
+        <v-col cols="auto" class="fill-height">
           <LoginMenu />
         </v-col>
       </v-row>
@@ -68,6 +71,31 @@ export default Navbar;
 </script>
 
 <style lang="scss">
+@import '@/plugins/vuetify/vuetify';
 .Navbar {
+  &__nav {
+    &-item {
+      display: flex;
+      flex-direction: column;
+
+      .v-btn {
+        height: unset !important;
+        flex: 1 1 auto;
+      }
+    }
+  }
+
+  .title {
+    color: white;
+  }
+  .col {
+    @extend .py-0;
+    display: flex;
+    align-items: center;
+  }
+
+  .v-toolbar__content {
+    @extend .py-0;
+  }
 }
 </style>
