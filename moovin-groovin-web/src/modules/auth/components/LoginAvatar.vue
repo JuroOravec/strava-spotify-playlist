a9bbc2821cfe433bb358f6fc1089b3cf00e4ca5e
Juro Oravec <juraj.oravec.josefson@gmail.com>
2021-01-30 13:26:43 +0000

    refactor: finish login components

--- moovin-groovin-web/src/modules/auth/components/LoginAvatar.vue
+++ moovin-groovin-web/src/modules/auth/components/LoginAvatar.vue
@@ -1,21 +1,93 @@
 <template>
-  <v-btn class="LoginAvatar" outlined color="#dddddd" v-bind="$attrs" v-on="$listeners">
-    LOGIN
-  </v-btn>
+  <div class="LoginAvatar" :class="{ 'LoginAvatar--logged-in': user }">
+    <v-btn
+      v-if="!user"
+      class="LoginAvatar__avatar"
+      outlined
+      color="#dddddd"
+      v-bind="$attrs"
+      v-on="$listeners"
+    >
+      LOGIN
+    </v-btn>
+    <v-btn
+      v-else
+      class="LoginAvatar__avatar"
+      outlined
+      fab
+      small
+      color="#dddddd"
+      v-bind="$attrs"
+      v-on="$listeners"
+    >
+      <v-avatar size="36">
+        <v-img v-if="user.photo" :src="user.photo" contain>
+          <div class="LoginAvatar__avatar-overlay transition-fast-in-fast-out" />
+        </v-img>
+        <span v-else>
+          {{ userInitials }}
+        </span>
+      </v-avatar>
+    </v-btn>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import { computed, defineComponent, unref } from '@vue/composition-api';
 
-const LoginAvatar = defineComponent({
+import useCurrentUser from '../composables/useCurrentUser';
+
+export default defineComponent({
   name: 'LoginAvatar',
   inheritAttrs: false,
-});
+  setup() {
+    const { user } = useCurrentUser();
 
-export default LoginAvatar;
+    const userInitials = computed(() => {
+      const { nameFamily, nameGiven, nameDisplay } = unref(user) ?? {};
+
+      const names = nameFamily && nameGiven ? [nameGiven, nameFamily] : nameDisplay?.split(' ');
+      if (!names?.length) return 'You';
+      return names.map((name) => name[0]).join('');
+    });
+
+    return {
+      user,
+      userInitials,
+    };
+  },
+});
 </script>
 
 <style lang="scss">
-.LoginAvatar {
+@import '@/plugins/vuetify/vuetify';
+$name: LoginAvatar;
+
+.#{$name} {
+  &__avatar {
+    border-width: 2px;
+
+    &-overlay {
+      @extend .transition-fast-in-fast-out;
+
+      height: 100%;
+      width: 100%;
+
+      &:hover {
+        background-color: rgba(255, 255, 255, 0.2);
+      }
+    }
+  }
+
+  &--logged-in {
+    .#{$name}__avatar {
+      border-radius: 50px;
+
+      .v-btn__content {
+        @extend .body-2;
+        font-weight: bold !important;
+      }
+    }
+  }
 }
 </style>
--- moovin-groovin-web/src/modules/auth/components/LoginMenu.vue
+++ moovin-groovin-web/src/modules/auth/components/LoginMenu.vue
@@ -3,13 +3,20 @@
     <template #activator="{ on, attrs }">
       <LoginAvatar v-bind="attrs" v-on="on" />
     </template>
-    <LoginProviders />
+    <LoginProviders v-if="!user" />
+
+    <v-list v-else>
+      <v-list-item class="LoginProviders__login" link @click="logout">
+        <v-list-item-title> Logout </v-list-item-title>
+      </v-list-item>
+    </v-list>
   </v-menu>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
 
+import useCurrentUser from '../composables/useCurrentUser';
 import LoginAvatar from './LoginAvatar.vue';
 import LoginProviders from './LoginProviders.vue';
 
@@ -20,6 +27,14 @@ const LoginMenu = defineComponent({
     LoginProviders,
   },
   inheritAttrs: false,
+  setup() {
+    const { user, logout } = useCurrentUser();
+
+    return {
+      user,
+      logout,
+    };
+  },
 });
 
 export default LoginMenu;
