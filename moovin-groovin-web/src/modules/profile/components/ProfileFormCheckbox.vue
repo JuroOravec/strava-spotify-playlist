e06158a722cfd432d42bcf07ffa8da319b0c5c05
Juro Oravec <juraj.oravec.josefson@gmail.com>
2021-02-06 11:23:28 +0000

    feat: add components for profile preferences

---
+++ moovin-groovin-web/src/modules/profile/components/ProfileFormCheckbox.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="ProfileFormCheckbox">
+    <v-checkbox v-bind="$attrs" :persistent-hint="true" v-on="$listeners">
+      <template #label>
+        <div class="ProfileFormCheckbox__label">
+          <slot name="label" />
+        </div>
+      </template>
+    </v-checkbox>
+    <p class="ProfileFormCheckbox__label-detail">
+      <slot name="label-detail" />
+    </p>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+const ProfileFormCheckbox = defineComponent({
+  name: 'ProfileFormCheckbox',
+  inheritAttrs: false,
+});
+
+export default ProfileFormCheckbox;
+</script>
+
+<style lang="scss">
+@import '@/plugins/vuetify/vuetify';
+.ProfileFormCheckbox {
+  &__label {
+    @extend .text-body-1;
+    color: black;
+  }
+
+  &__label-detail {
+    @extend .text-caption, .mb-0, .pl-8;
+  }
+
+  .v-messages {
+    display: none;
+  }
+
+  .v-input__slot {
+    margin: 0;
+    align-items: start;
+  }
+}
+</style>
---
+++ moovin-groovin-web/src/modules/profile/components/ProfileFormTextarea.vue
@@ -0,0 +1,71 @@
+<template>
+  <ProfileCheckbox
+    class="ProfileFormTextarea"
+    :class="{ 'ProfileFormTextarea--no-checkbox': !checkbox }"
+  >
+    <template #label>
+      <slot name="label" />
+    </template>
+    <template #label-detail>
+      <slot name="label-detail" />
+    </template>
+    <v-textarea
+      rows="3"
+      auto-grow
+      filled
+      counter
+      clearable
+      persistent-hint
+      :validate-on-blur="true"
+      clear-icon="mdi-close-circle"
+      class="ProfileFormTextarea__textarea"
+    />
+  </ProfileCheckbox>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+import ProfileCheckbox from './ProfileFormCheckbox.vue';
+
+const ProfileFormTextarea = defineComponent({
+  name: 'ProfileFormTextarea',
+  components: {
+    ProfileCheckbox,
+  },
+  props: {
+    checkbox: { type: Boolean, default: false },
+  },
+});
+
+export default ProfileFormTextarea;
+</script>
+
+<style lang="scss">
+@import '~@/plugins/vuetify/vuetify';
+.ProfileFormTextarea {
+  &__textarea {
+    @extend .pl-8;
+  }
+
+  .v-text-field__details {
+    @extend .pt-2;
+    margin-bottom: 0 !important;
+    flex-direction: row-reverse;
+  }
+
+  &--no-checkbox {
+    .ProfileFormTextarea__textarea {
+      padding: 0 !important;
+    }
+
+    .v-input--selection-controls__input {
+      display: none;
+    }
+
+    .ProfileFormCheckbox__label-detail {
+      padding-left: 0 !important;
+    }
+  }
+}
+</style>
