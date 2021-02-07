3bb7220aa0576f38f77377e321fa813126b87f60
Juro Oravec <juraj.oravec.josefson@gmail.com>
2021-02-07 21:14:40 +0000

    feat: add textarea component for input on profile page

--- moovin-groovin-web/src/modules/profile/components/ProfileFormTextarea.vue
+++ moovin-groovin-web/src/modules/profile/components/ProfileFormTextarea.vue
@@ -1,7 +1,26 @@
 <template>
-  <ProfileCheckbox
+  <ProfileFormCheckbox
     class="ProfileFormTextarea"
-    :class="{ 'ProfileFormTextarea--no-checkbox': !checkbox }"
+    :class="{
+      'ProfileFormTextarea--no-checkbox': !checkbox,
+      'ProfileFormTextarea--disabled': checkbox && !checkboxInputValue,
+    }"
+    :input-value="checkboxInputValue"
+    v-on="
+      prefixEvents(
+        [
+          'change',
+          'click',
+          'click:append',
+          'click:prepend',
+          'mousedown',
+          'mouseup',
+          'update:error',
+          'update:indeterminate',
+        ],
+        'checkbox-'
+      )
+    "
   >
     <template #label>
       <slot name="label" />
@@ -14,27 +33,76 @@
       auto-grow
       filled
       counter
-      clearable
       persistent-hint
       :validate-on-blur="true"
-      clear-icon="mdi-close-circle"
+      :disabled="checkbox && !checkboxInputValue"
+      :value="value"
       class="ProfileFormTextarea__textarea"
-    />
-  </ProfileCheckbox>
+      v-bind="$attrs"
+      v-on="{
+        ...$listeners,
+        input: onTextareaInput,
+      }"
+    >
+      <template v-if="textareaIsDirty" #append>
+        <v-btn icon x-small @click="$emit('cancel')">
+          <v-icon>cancel</v-icon>
+        </v-btn>
+      </template>
+    </v-textarea>
+  </ProfileFormCheckbox>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import { computed, defineComponent, ref, toRefs, unref, watch, Ref } from '@vue/composition-api';
 
-import ProfileCheckbox from './ProfileFormCheckbox.vue';
+import useListeners from '@/modules/utils/composables/useListeners';
+import ProfileFormCheckbox from './ProfileFormCheckbox.vue';
 
 const ProfileFormTextarea = defineComponent({
   name: 'ProfileFormTextarea',
   components: {
-    ProfileCheckbox,
+    ProfileFormCheckbox,
   },
+  inheritAttrs: false,
   props: {
     checkbox: { type: Boolean, default: false },
+    checkboxInputValue: { type: Boolean, default: false },
+    value: { type: String, default: undefined },
+    defaultValue: { type: String, default: undefined },
+  },
+  setup(props, { emit }) {
+    const { value, defaultValue } = toRefs(props);
+
+    const textareaValue: Ref<string | undefined> = ref();
+
+    ///////////////////////////////////////////////////////////
+    // Detect when textarea value differs from default value
+    ///////////////////////////////////////////////////////////
+
+    watch(
+      value,
+      (newValue) => {
+        textareaValue.value = newValue;
+      },
+      { immediate: true }
+    );
+
+    const { interceptEvent, prefixEvents } = useListeners({ emit });
+
+    const onTextareaInput = interceptEvent('input', (_, __, newValue?: string) => {
+      textareaValue.value = newValue;
+    });
+
+    const textareaIsDirty = computed((): boolean =>
+      Boolean(unref(defaultValue) && unref(textareaValue) !== unref(defaultValue))
+    );
+
+    return {
+      onTextareaInput,
+      textareaIsDirty,
+      prefixEvents,
+    };
   },
 });
 
@@ -52,6 +120,15 @@ export default ProfileFormTextarea;
     @extend .pt-2;
     margin-bottom: 0 !important;
     flex-direction: row-reverse;
+  }
+
+  .v-input__append-inner {
+    margin-top: 10px !important;
+  }
+
+  textarea {
+    @extend .text-body-2;
+    margin-bottom: 10px !important;
   }
 
   &--no-checkbox {
