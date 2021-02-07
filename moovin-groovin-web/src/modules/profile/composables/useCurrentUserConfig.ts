36bb093b0a5acb93e9894fd98c43caab3729d812
Juro Oravec <juraj.oravec.josefson@gmail.com>
2021-02-07 21:12:09 +0000

    feat: add composable for currentUserConfig

---
+++ moovin-groovin-web/src/modules/profile/composables/useCurrentUserConfig.ts
@@ -0,0 +1,124 @@
+import { computed, ref, unref, watch, ComputedRef, Ref, readonly } from '@vue/composition-api';
+import { ApolloQueryResult, gql } from '@apollo/client/core';
+import clone from 'lodash/clone';
+import memoize from 'lodash/memoize';
+
+import {
+  updateCurrentUserConfigMutationCompositionFunctionResult,
+  usegetCurrentUserConfigQuery,
+  useupdateCurrentUserConfigMutation,
+} from '@/plugins/apollo/composables';
+import type { GqlgetCurrentUserConfigQuery } from '@/plugins/apollo/types';
+
+interface UserConfig {
+  playlistCollaborative: boolean;
+  playlistPublic: boolean;
+  playlistAutoCreate: boolean;
+  playlistDescriptionTemplate: string | null;
+  playlistTitleTemplate: string | null;
+  activityDescriptionEnabled: boolean;
+  activityDescriptionTemplate: string | null;
+}
+
+interface UseCurrentUserConfig {
+  config: ComputedRef<Readonly<UserConfig | null>>;
+  loading: ComputedRef<boolean>;
+  refetch: () => Promise<ApolloQueryResult<GqlgetCurrentUserConfigQuery>>;
+  updateConfig: updateCurrentUserConfigMutationCompositionFunctionResult['mutate'];
+}
+
+// ====================================================
+// Query
+// ====================================================
+
+const GET_CURRENT_USER_CONFIG = gql`
+  query getCurrentUserConfig {
+    getCurrentUserConfig {
+      playlistCollaborative
+      playlistPublic
+      playlistAutoCreate
+      playlistDescriptionTemplate
+      playlistTitleTemplate
+      activityDescriptionEnabled
+      activityDescriptionTemplate
+    }
+  }
+`;
+
+gql`
+  mutation updateCurrentUserConfig($userConfigInput: UserConfigInput!) {
+    updateCurrentUserConfig(userConfigInput: $userConfigInput) {
+      playlistCollaborative
+      playlistPublic
+      playlistAutoCreate
+      playlistDescriptionTemplate
+      playlistTitleTemplate
+      activityDescriptionEnabled
+      activityDescriptionTemplate
+    }
+  }
+`;
+
+// ====================================================
+// Composable
+// ====================================================
+
+const transformUserConfig = (
+  configData?: GqlgetCurrentUserConfigQuery['getCurrentUserConfig']
+): UserConfig | null => {
+  if (!configData) return null;
+  const { __typename, ...config } = configData;
+  return config;
+};
+
+const useCurrentUserConfig = (): UseCurrentUserConfig => {
+  // Apollo client cache is not Vue-reactive, so when we memoize this composable,
+  // pushing new data to the cache won't update the result of query.
+  // To work around that, we keep a copy of the state that's sync'd to the cache.
+  const configModel: Ref<UserConfig | null> = ref(null);
+
+  const {
+    result: configResult,
+    loading: loadingGetConfig,
+    refetch: refetchConfig,
+  } = usegetCurrentUserConfigQuery({
+    fetchPolicy: 'cache-and-network',
+  });
+
+  const { mutate: updateConfig, loading: loadingUpdateConfig } = useupdateCurrentUserConfigMutation(
+    {
+      update: (cache, result) => {
+        const newConfig = transformUserConfig(result.data?.updateCurrentUserConfig);
+
+        cache.writeQuery({
+          query: GET_CURRENT_USER_CONFIG,
+          data: { getCurrentUserConfig: clone(newConfig) },
+        });
+
+        configModel.value = clone(newConfig);
+      },
+    }
+  );
+
+  watch(configResult, (newConfigResult) => {
+    const config = transformUserConfig(newConfigResult?.getCurrentUserConfig);
+    configModel.value = clone(config);
+  });
+
+  const isUserConfigLoading = computed(
+    (): boolean => unref(loadingGetConfig) || unref(loadingUpdateConfig)
+  );
+
+  // NOTE: DO NOT USE `computed` to props read-only when memoizing the composable.
+  // It does not work together.
+  return {
+    config: readonly(configModel),
+    loading: isUserConfigLoading,
+    refetch: refetchConfig,
+    updateConfig,
+  };
+};
+
+export default memoize(useCurrentUserConfig);
+export { GET_CURRENT_USER_CONFIG };
+export type { UserConfig };
