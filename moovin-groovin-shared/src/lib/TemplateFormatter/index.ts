0fd9bbcb2fa46f6a6b49afed70b74c40eb5a94bf
Juro Oravec <juraj.oravec.josefson@gmail.com>
2021-02-08 23:49:24 +0000

    refactor: add index files to shared files to support top-level import

--- moovin-groovin-shared/src/lib/TemplateFormatter/index.ts
+++ moovin-groovin-shared/src/lib/TemplateFormatter/index.ts
@@ -1,1 +1,4 @@
 export { default as HandlebarsTemplateFormatter } from './HandlebarsTemplateFormatter';
+export { default as validateTemplate } from './utils/validateTemplate';
+export { default as setOnMissingPropStringifierProxy } from './utils/setOnMissingPropStringifierProxy';
+export type { TemplateFormatter } from './types';
