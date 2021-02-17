91d46f538b12ddb60391ed2f70da0f3d9eae8d1e
Juro Oravec <juraj.oravec.josefson@gmail.com>
2021-02-17 21:00:32 +0000

    feat: re-add footer

--- moovin-groovin-web/src/modules/base/components/AppFooter.vue
+++ moovin-groovin-web/src/modules/base/components/AppFooter.vue
@@ -1,12 +1,39 @@
 <template>
-  <v-footer class="AppFooter" app dark absolute> Save the songs that keep you moving. </v-footer>
+  <v-footer class="AppFooter" app absolute>
+    <v-row class="AppFooter__container text-body-2">
+      <v-col cols="auto">
+        <router-link :to="{ name: BaseRoute.PRIVACY_POLICY }" class="AppFooter__link"
+          >Privacy</router-link
+        >
+      </v-col>
+      <v-col cols="auto" class="px-0"> ∙ </v-col>
+      <v-col cols="auto">
+        Created by
+        <a
+          href="https://github.com/JuroOravec"
+          target="_blank"
+          rel="noopener"
+          class="AppFooter__link"
+        >
+          Juro Oravec
+        </a>
+      </v-col>
+    </v-row>
+  </v-footer>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
 
+import { BaseRoute } from '../types';
+
 const AppFooter = defineComponent({
   name: 'AppFooter',
+  setup() {
+    return {
+      BaseRoute,
+    };
+  },
 });
 
 export default AppFooter;
@@ -14,6 +41,11 @@ export default AppFooter;
 
 <style lang="scss">
 .AppFooter {
-  display: none;
+  &__container {
+    justify-content: center;
+  }
+  &__link {
+    font-weight: bold;
+  }
 }
 </style>
