20701fa35dfc713afe198568f1c117ff2589f18e
Juro Oravec <juraj.oravec.josefson@gmail.com>
2021-02-23 22:20:31 +0000

    feat: add icons, groups, and listeners to ProfileMenu items

--- moovin-groovin-web/src/modules/profile/components/ProfileMenu.vue
+++ moovin-groovin-web/src/modules/profile/components/ProfileMenu.vue
@@ -1,45 +1,87 @@
 <template>
-  <v-card class="ProfileMenu" rounded="lg">
-    <v-list color="transparent">
-      <router-link
-        v-for="section in menuSections"
-        :key="'menu-' + section.sectionId"
-        :to="section.to"
-      >
-        <v-list-item link>
-          <v-list-item-content>
-            <v-list-item-title>
-              {{ section.title }}
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </router-link>
-    </v-list>
-  </v-card>
+  <div class="ProfileMenu">
+    <v-card
+      v-for="[groupId, menuSectionGroup] in menuSectionGroups"
+      :key="groupId"
+      class="ProfileMenu__group"
+    >
+      <v-navigation-drawer floating permanent>
+        <v-list color="transparent" class="py-0">
+          <v-list-item
+            v-for="section in menuSectionGroup"
+            :key="'menu-' + section.sectionId"
+            link
+            :to="section.to"
+            class="ProfileMenu__item"
+            v-on="section.listeners"
+          >
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ section.title }}
+              </v-list-item-title>
+              <v-icon v-if="section.iconAppend" small class="flex"
+                >{{ section.iconAppend }}
+              </v-icon>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </v-card>
+    <MembershipOfferDialog
+      :value="offerDialogIsOpen"
+      @cancel="setOfferDialogIsOpen(false)"
+      @confirm="setOfferDialogIsOpen(false)"
+    >
+      <template #activator>
+        <div />
+      </template>
+    </MembershipOfferDialog>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
 import type { Location } from 'vue-router';
+import find from 'lodash/find';
+import groupBy from 'lodash/groupBy';
 
+import useRefRich from '@/modules/utils-reactivity/composables/useRefRich';
 import { ProfileRoute } from '../types';
+import MembershipOfferDialog from './MembershipOfferDialog.vue';
 
-const menuSections: {
+type MenuSectionGroups = 'dashboard' | 'settings';
+
+interface MenuSection {
+  groupId: MenuSectionGroups;
   sectionId: string;
   title: string;
-  to: Location & { name: ProfileRoute };
-}[] = [
+  to?: Location & { name: ProfileRoute };
+  listeners?: Record<string, (...args: any[]) => void>;
+  iconAppend?: string;
+}
+
+const menuSections: MenuSection[] = [
   {
+    groupId: 'dashboard',
+    sectionId: 'dashboard',
+    title: 'Dashboard',
+    listeners: { click: console.log },
+    iconAppend: 'lock',
+  },
+  {
+    groupId: 'settings',
     sectionId: ProfileRoute.PREFERENCES,
     title: 'Preferences',
     to: { name: ProfileRoute.PREFERENCES },
   },
   {
+    groupId: 'settings',
     sectionId: ProfileRoute.INTEGRATIONS,
     title: 'Connected Apps',
     to: { name: ProfileRoute.INTEGRATIONS },
   },
   {
+    groupId: 'settings',
     sectionId: ProfileRoute.ACCOUNT,
     title: 'Manage My Account',
     to: { name: ProfileRoute.ACCOUNT },
@@ -48,9 +90,34 @@ const menuSections: {
 
 const ProfileMenu = defineComponent({
   name: 'ProfileMenu',
+  components: {
+    MembershipOfferDialog,
+  },
   setup: () => {
+    const { ref: offerDialogIsOpen, setter: setOfferDialogIsOpen } = useRefRich({ value: false });
+
+    const menuSectionListeners: Pick<MenuSection, 'groupId' | 'sectionId' | 'listeners'>[] = [
+      {
+        groupId: 'dashboard',
+        sectionId: 'dashboard',
+        listeners: { click: () => setOfferDialogIsOpen(true) },
+      },
+    ];
+
+    const menuSectionGroups = Object.entries(
+      groupBy(
+        menuSections.map((section) => ({
+          ...section,
+          ...find(menuSectionListeners, { sectionId: section.sectionId, groupId: section.groupId }),
+        })),
+        (section) => section.groupId
+      )
+    );
+
     return {
-      menuSections,
+      menuSectionGroups,
+      offerDialogIsOpen,
+      setOfferDialogIsOpen,
     };
   },
 });
@@ -60,6 +127,20 @@ export default ProfileMenu;
 
 <style lang="scss">
 .ProfileMenu {
+  &__group {
+    & + & {
+      margin-top: 7 * $spacer;
+    }
+  }
+  &__item {
+    .v-list-item__title,
+    .v-icon {
+      flex: 0 1 auto;
+    }
+    .v-list-item__content {
+      justify-content: space-between;
+    }
+  }
   .v-list-item + .v-list-item {
     border-top: 1px solid lightgrey;
   }
