7d4555dcefbb8cdb3696b1eff2b90eee7644eda0
Juro Oravec <juraj.oravec.josefson@gmail.com>
2021-02-23 22:16:18 +0000

    feat: add dirtiness tracking to useFormData

--- moovin-groovin-web/src/modules/utils/composables/useFormData.ts
+++ moovin-groovin-web/src/modules/utils/composables/useFormData.ts
@@ -3,15 +3,25 @@ import defaults from 'lodash/defaults';
 import pickBy from 'lodash/pickBy';
 import reduce from 'lodash/reduce';
 import isEqual from 'lodash/isEqual';
+import mapValues from 'lodash/mapValues';
 
 import type OptionalRef from '@/modules/utils-reactivity/types/OptionalRef';
 
-/* eslint-disable-next-line @typescript-eslint/ban-types */
-interface UseFormData<T extends object> {
+interface UseFormData<
+  /* eslint-disable @typescript-eslint/ban-types */
+  T extends object,
+  TSerialized extends object = Partial<Record<keyof T, string>>
+  /* eslint-enable */
+> {
   /** Form Data */
   formData: Ref<Partial<T>>;
+  formDataSerialized: ComputedRef<Partial<TSerialized>>;
   updateFormData: (updates: Partial<T>) => void;
   resetFormData: (keysToReset?: (keyof T)[]) => void;
+  /** Object of form data keys that have been modified */
+  dirtys: Ref<Partial<Record<keyof T, boolean>>>;
+  /** Whether form data has been modified */
+  isDirty: Ref<boolean>;
   /** Defaults that we can revert the form data to. Used for resetting form state. */
   hasChanges: ComputedRef<boolean>;
   /**
@@ -21,20 +31,33 @@ interface UseFormData<T extends object> {
   hasUnconfirmedChanges: Ref<boolean>;
 }
 
-/* eslint-disable-next-line @typescript-eslint/ban-types */
-const useFormData = <T extends object>(
+const useFormData = <
+  /* eslint-disable @typescript-eslint/ban-types */
+  T extends object,
+  TSerialized extends object = Record<keyof T, string>
+  /* eslint-enable */
+>(
   options: {
     /** Defaults that we can revert the form data to. Used for resetting form state. */
     defaults?: OptionalRef<Partial<T> | null>;
+    serializer?: (data: Partial<T>) => Partial<TSerialized>;
   } = {}
-): UseFormData<T> => {
-  const { defaults: dataDefaults } = options;
+): UseFormData<T, TSerialized> => {
+  const {
+    defaults: dataDefaults,
+    serializer = (data) => mapValues(data, (val) => val + ''),
+  } = options;
 
   const formData: Ref<Partial<T>> = ref({});
   const defaultsRef = ref(dataDefaults) as Ref<Partial<T> | undefined>;
+  const dirtys: Ref<Partial<Record<keyof T, boolean>>> = ref({});
   const hasUnconfirmedChanges = ref(false);
 
   const hasChanges = computed((): boolean => !isEqual(unref(defaultsRef) ?? {}, unref(formData)));
+  const isDirty = computed((): boolean => Boolean(Object.keys(unref(dirtys) || {}).length));
+  const formDataSerialized = computed(
+    (): Partial<TSerialized> => serializer(unref(formData)) as Partial<TSerialized>
+  );
 
   watch(
     hasChanges,
@@ -64,6 +87,7 @@ const useFormData = <T extends object>(
 
   const updateFormData = (updates: Partial<T>): void => {
     formData.value = { ...unref(formData), ...updates };
+    dirtys.value = { ...unref(dirtys), ...mapValues(updates, () => true) };
   };
 
   const resetFormData = (keysToReset?: (keyof T)[]): void => {
@@ -79,10 +103,14 @@ const useFormData = <T extends object>(
     );
 
     formData.value = { ...unref(formData), ...overrides };
+    dirtys.value = {};
   };
 
   return {
     formData,
+    formDataSerialized,
+    dirtys,
+    isDirty,
     hasChanges,
     hasUnconfirmedChanges,
     updateFormData,
