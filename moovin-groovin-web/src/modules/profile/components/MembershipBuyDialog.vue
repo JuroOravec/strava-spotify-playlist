9fc8cc8b92a4fb8e263e40c2bb1dd3ca0c4da4cb
Juro Oravec <juraj.oravec.josefson@gmail.com>
2021-02-23 22:23:34 +0000

    feat: add MailerLite sign up form for product newsletter

--- moovin-groovin-web/src/modules/profile/components/MembershipBuyDialog.vue
+++ moovin-groovin-web/src/modules/profile/components/MembershipBuyDialog.vue
@@ -1,5 +1,9 @@
 <template>
-  <ConfirmDialog v-bind="$attrs" content-class="MembershipBuyDialog" v-on="$listeners">
+  <ConfirmDialog
+    v-bind="$attrs"
+    content-class="MembershipBuyDialog"
+    v-on="{ ...$listeners, confirm: onConfirmSubmit }"
+  >
     <template #activator="{ on, attrs }">
       <slot name="activator" v-bind="{ on, attrs }">
         <v-btn color="primary" v-bind="attrs" v-on="on">
@@ -16,14 +20,59 @@
         </v-col>
       </v-row>
       <v-row>
-        <v-col class="MembershipBuyDialog__title">
+        <v-col>
           <v-card-text>
-            <h3 class="text-h5 pt-2 px-2">The membership features are still in development</h3>
+            <p class="text-body-1 font-weight-bold pt-2 px-2">
+              The membership features are still in development.
+            </p>
+            <p class="text-body-1 py-2 px-2">
+              Subscribe to receive product updates and have a say in product roadmap.
+            </p>
+            <v-form data-code="i3l3f1">
+              <v-text-field
+                type="text"
+                autocomplete="name"
+                label="Name"
+                class="text-body-2"
+                aria-label="email"
+                aria-required="true"
+                filled
+                dense
+                validate-on-blur
+                :error-messages="formDataErrors.name"
+                @input="(name) => updateFormData({ name })"
+                @blur="updateFormData({ name: $event.target.value })"
+              />
+              <v-text-field
+                id="email"
+                type="email"
+                autocomplete="email"
+                label="Email"
+                hint="name@example.com"
+                class="text-body-2"
+                aria-label="email"
+                aria-required="true"
+                filled
+                dense
+                :error-messages="formDataErrors.email"
+                @input="(email) => updateFormData({ email })"
+                @blur="updateFormData({ email: $event.target.value })"
+              />
+            </v-form>
           </v-card-text>
         </v-col>
       </v-row>
     </template>
-    <template #confirm-text><span class="text-body-1">Buy membership</span></template>
+    <template #confirm-action="{ confirm }">
+      <v-btn
+        class="text-body-1"
+        color="primary"
+        :disabled="!formIsValid || !isDirty"
+        @click="confirm"
+      >
+        Subscribe
+      </v-btn>
+    </template>
     <template #cancel-action="{ cancel }">
       <v-btn color="primary" outlined @click="cancel"> Cancel </v-btn>
     </template>
@@ -31,9 +80,15 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import { defineComponent, onMounted, unref } from '@vue/composition-api';
+import * as Sentry from '@sentry/vue';
 
 import ConfirmDialog from '@/modules/utils/components/ConfirmDialog.vue';
+import useAnalytics from '@/plugins/analytics/composables/useAnalytics';
+import useFormData from '@/modules/utils/composables/useFormData';
+import useValidators from '@/modules/utils/composables/useValidators';
+import * as validators from '@/modules/utils/utils/validators';
+import useListeners from '@/modules/utils/composables/useListeners';
 
 const MembershipBuyDialog = defineComponent({
   name: 'MembershipBuyDialog',
@@ -41,8 +96,60 @@ const MembershipBuyDialog = defineComponent({
     ConfirmDialog,
   },
   inheritAttrs: false,
-  setup() {
-    return {};
+  setup(_, { emit }) {
+    const { trackEvent } = useAnalytics();
+
+    const { interceptEvent } = useListeners({ emit });
+
+    const {
+      formData: signupFormData,
+      formDataSerialized: serializedSignupFormData,
+      updateFormData,
+      isDirty,
+    } = useFormData<
+      {
+        name: string;
+        email: string;
+      },
+      { 'fields[email]': string; 'fields[name]': string }
+    >({
+      defaults: { name: undefined, email: undefined },
+      serializer: (data) => ({ 'fields[email]': data.email, 'fields[name]': data.name }),
+    });
+
+    const { isValid: formIsValid, errors: formDataErrors } = useValidators(signupFormData, {
+      name: [validators.required],
+      email: [validators.required, validators.email],
+    });
+
+    onMounted(() => trackEvent('membership_interest'));
+
+    const onConfirmSubmit = interceptEvent('confirm', () => {
+      const formData = new FormData();
+      Object.entries(unref(serializedSignupFormData)).forEach(([key, val]) =>
+        formData.set(key, val ?? '')
+      );
+      // Not sure if these 2 are required, but they are on the singup form by ML
+      formData.set('ml-submit', '1');
+      formData.set('anticsrf', 'true');
+
+      fetch('https://static.mailerlite.com/webforms/submit/i3l3f1', {
+        method: 'POST',
+        body: formData,
+      }).catch((err) => {
+        if (!err) return;
+        console.error(err);
+        Sentry.captureException(err);
+      });
+    });
+
+    return {
+      formIsValid,
+      isDirty,
+      formDataErrors,
+      updateFormData,
+      onConfirmSubmit,
+    };
   },
 });
 
@@ -50,6 +157,7 @@ export default MembershipBuyDialog;
 </script>
 
 <style lang="scss">
+@import '~vuetify/src/styles/main';
 .MembershipBuyDialog {
   max-width: 700px;
   overflow: visible;
@@ -65,6 +173,17 @@ export default MembershipBuyDialog;
   .row {
     justify-content: center;
     text-align: center;
+  }
+
+  .v-input {
+    &.v-text-field {
+      max-width: 350px;
+      margin: auto;
+    }
+
+    .v-label {
+      @extend .text-body-2;
+    }
   }
 }
 </style>
---
+++ moovin-groovin-web/src/modules/utils/utils/validators.ts
@@ -0,0 +1,6 @@
+export const required = (value?: string): string | null => (!value ? 'Required.' : null);
+
+export const email = (value?: string): string | true => {
+  const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return pattern.test(value ?? '') || 'Invalid e-mail.';
+};
